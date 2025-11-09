77ca0b07fa110bdce1b1c787abf8f5e9f209cd7d
rybinagpt <rybinagpt@gmail.com>
2025-11-09 23:13:26 +0200

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
